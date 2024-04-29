5ba803ecc22a9d8805a589c025ceedac56462226
Mahmud Toshtemirov <mahmuddev1908@gmail.com>
2024-04-29 21:18:19 +0500

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/js/**/*.js",
+  ],
+
   theme: {
     extend: {},
+    screens: {
+      xs: "320px",
+      ss: "608px",
+      sm: "708px",
+      md: "1000px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
-  plugins: [],
+  plugins: [require("tw-elements/plugin.cjs")],
 };
