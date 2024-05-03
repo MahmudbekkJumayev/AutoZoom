a05ce2d4ece12d3256584b54987f296181dcf55d
Mahmud Toshtemirov <mahmuddev1908@gmail.com>
2024-05-03 10:36:10 +0500

    Global cars

---
+++ src/components/Location/Location.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Location = () => {
+  return (
+    <div>
+      <div className="container flex">
+        <div>
+          <h4 className="text-[40px] text-gray-100 font-bold">LOCATION</h4>
+          <ul></ul>
+        </div>
+        <div>
+          <h4 className="text-[40px] text-gray-100 font-bold">CITY</h4>
+          <ul></ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Location;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Carousel from "../../components/Carousel/Carousel";
 import Header from "../../components/Header/Header";
 import Brends from "../../components/Brends/Brends";
+import Global from "../../components/Global/Global";
+import SuperCar from "../../components/SuperCar/SuperCar";
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Header />
       <Brends />
       <Carousel />
+      <Global />
+      <SuperCar />
     </div>
   );
 };
