51ee6d2efa6ce2b5b10a9908bf7b65bf8ec2c6fa
Nazarov Behruzbek <behruzbeknazarov001@gmail.com>
2024-05-08 17:15:45 +0500

    carsinfo

--- src/components/CarsInfo/CarsInfo.jsx
+++ src/components/CarsInfo/CarsInfo.jsx
@@ -10,6 +10,11 @@ import './style.css';
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { CiCalendar } from "react-icons/ci";
+import { CiAlarmOn } from "react-icons/ci";
+import { IoMdSpeedometer } from "react-icons/io";
+import { IoPersonSharp } from "react-icons/io5";
+
 export default function CarsInfo() {
     const { id } = useParams();
     const urlimg = "https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/";
@@ -88,7 +93,7 @@ export default function CarsInfo() {
                         {cars &&
                             cars.map((item, index) => (
                                 <>
-                                {
+                                    {
                                         item.car_images.map((item, index) => (
                                             <SwiperSlide key={index} className='cars-swiper-two'>
                                                 <img src={`${urlimg}${item.image.src}`} alt={`Car ${index}`} />
@@ -102,7 +107,61 @@ export default function CarsInfo() {
 
 
                 </Col>
-                <Col md={12}></Col>
+                <Col md={12}>
+                    {cars &&
+                        cars.map((item, index) => (
+                            <>
+                                <div>
+                                    <div className='flex justify-between'>
+                                        <div>
+                                            <h3 className="text-white text-2xl font-bold  "><span>AED {item.price_in_aed}</span> <span className="text-gray-500">/ $ {item.price_in_usd}</span></h3>
+                                            <p className='text-gray-500 text-[13px]'>per day</p>
+                                            <h3 className="text-white text-xl  "><del><span>AED</span> <span className="text-gray-500">/ $ </span></del></h3>
+                                        </div>
+                                        <div>
+                                            <p className=' text-[15px]   text-gray-500'>Deposit</p>
+                                            <p className='pb-6'></p>
+                                            <p className='text-gray-500 text-[13px]'>Premium protection</p>
+                                        </div>
+                                        <div>
+                                            <h4 className='text-white text-xl font-bold  '>AED 0</h4>
+                                            <p className='text-gray-500 text-[13px] pb-3'>AED 0 for credit cards payment</p>
+                                            <h3 className="text-white text-xl font-bold  "><span>AED {item.price_in_aed}</span></h3>
+                                            <p className='text-gray-500 text-[13px]'>AED {item.price_in_aed} price for 1 day</p>
+                                        </div>
+
+                                    </div>
+                                    <hr className='text-gray-500' />
+                                </div>
+
+                                <div className='pt-8'>
+                                    <div className='flex justify-between'>
+                                        <div>
+                                            <span className='text-3xl text-gray-500'><CiCalendar /> </span>
+                                            <p> {item.year}</p>
+                                        </div>
+                                        <div>
+                                            <span className='text-3xl text-gray-500'>< CiAlarmOn /></span>
+                                            <p> {item.seconds}</p>
+                                        </div>
+                                        <div>
+                                            <span className='text-3xl text-gray-500'><IoMdSpeedometer /></span>
+                                            <p> {item.max_speed}</p>
+                                        </div>
+                                        <div>
+                                            <span className='text-3xl text-gray-500'><IoPersonSharp /></span>
+                                            <p> {item.max_people}</p>
+                                        </div>
+                                    </div>
+                                    
+                                    <hr className='text-gray-500' />
+                                </div>
+                            </>
+
+
+                        ))}
+
+                </Col>
             </Row>
         </div>
     )
